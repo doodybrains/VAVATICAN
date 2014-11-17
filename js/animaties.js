9bb4c92c98ec97a64e843cbc47e6b7fc95b1f911
doodybrains <doodybrainz@gmail.com>
2014-11-17 14:43:06 -0500

    Initial structure/styles

--- js/animaties.js
+++ js/animaties.js
@@ -1,13 +1,6 @@
 $( document ).ready(function() {
-  $(".button-close").hide();
-    $(".button").click(function(){
-      $(".button").hide();
-      $(".button-close").show();
-      $(".menu-box").show();
-    });
-    $(".button-close").click(function(){
-      $(".button-close").hide();
-      $(".button").show();
-      $(".menu-box").hide();
-    });
+  $('.open').click(function() {
+    console.log('hello');
+    $('.menu').toggleClass('active');
+  });
 });
