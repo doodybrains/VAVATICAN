94ff7d01bb524ceb5b985dc0d079f7002cb969a8
doodybrains <doodybrainz@gmail.com>
2014-11-17 19:11:43 -0500

    final structure - no cleanup

--- js/animaties.js
+++ js/animaties.js
@@ -1,6 +1,13 @@
 $( document ).ready(function() {
-  $('.open').click(function() {
-    console.log('hello');
-    $('.menu').toggleClass('active');
-  });
+  $(".button-close").hide();
+    $(".button").click(function(){
+      $(".button").hide();
+      $(".button-close").show();
+      $(".menu-box").show();
+    });
+    $(".button-close").click(function(){
+      $(".button-close").hide();
+      $(".button").show();
+      $(".menu-box").hide();
+    });
 });
