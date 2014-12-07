366330e2a6eef8a9e6a8e0974f654edebd66959b
doodybrains <doodybrainz@gmail.com>
2014-12-07 15:31:52 -0500

    Adjust button images - Add styles

--- js/animaties.js
+++ js/animaties.js
@@ -1,25 +1,25 @@
 $( document ).ready(function() {
     $(".button-close").hide();
     $(".button").click(function(){
-      $(".button").hide();
+      $(".button, .button2, .butt").hide();
       $(".button-close").show();
       $(".menu-box").show();
     });
     $(".button-close").click(function(){
       $(".button-close").hide();
-      $(".button").show();
+      $(".button, .butt").show();
       $(".menu-box").hide();
     });
 
-     $(".button-close-2").hide();
-      $(".button-2").click(function(){
-      $(".button-2").hide();
-      $(".button-close-2").show();
-      $(".menu-box-2").show();
+     $(".button-close").hide();
+      $(".button2").click(function(){
+      $(".button2, .button, .butt").hide();
+      $(".button-close").show();
+      $(".menu-box2").show();
     });
-    $(".button-close-2").click(function(){
-      $(".button-close-2").hide();
-      $(".button-2").show();
-      $(".menu-box-2").hide();
+    $(".button-close").click(function(){
+      $(".button-close").hide();
+      $(".button2, .butt").show();
+      $(".menu-box2").hide();
     });
 });
